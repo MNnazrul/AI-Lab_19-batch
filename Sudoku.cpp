09ce09c4bbd315c442c5551a1c56598e8a0b0b58
MNnazrul <mnnazrulislam196130@gmail.com>
2023-11-13 22:06:20 +0600

    sudoku

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -8,8 +8,10 @@ bool is_valid(int x, int y, int val) {
     for (int i = 0; i < 9; i++)
         if(grid[i][y] == val or grid[x][i] == val) return false;
 
-    for (int i = x/3; i < x/3 + 3; i++) {
-        for (int j = y/3; j < y/3 + 3; j++)
+    int i1 = (x/3) * 3, j1 = (y/3) * 3;
+
+    for (int i = i1; i < i1 + 3; i++) {
+        for (int j = j1; j < j1 + 3; j++)
             if(grid[i][j] == val) return false;
     }
 
@@ -17,7 +19,7 @@ bool is_valid(int x, int y, int val) {
 }
 
 bool sudoku(int id) {
-    if(id >= grid.size()) return true;
+    if(id >= faka.size()) return true;
 
     auto[i1, j1] = faka[id];
 
@@ -26,6 +28,7 @@ bool sudoku(int id) {
             grid[i1][j1] = i;
             if(sudoku(id+1)) return true;
         }
+        grid[i1][j1] = 0;
     }
     return false;
 }
@@ -34,15 +37,15 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-                grid = {{ 0, 0, 8, 0, 5, 0, 0, 0, 0},
-                       { 1, 0, 0, 9, 2, 0, 0, 0, 6},
-                       { 0, 6, 0, 0, 0, 0, 0, 7, 0},
-                       { 0, 0, 4, 0, 0, 0, 8, 0, 0},
-                       { 0, 0, 0, 0, 0, 3, 0, 0, 0},
-                       { 2, 0, 0, 1, 6, 0, 0, 0, 7},
-                       { 0, 0, 2, 3, 9, 0, 0, 4, 0},
-                       { 0, 0, 0, 0, 0, 5, 0, 0, 9},
-                       { 3, 0, 0, 0, 0, 7, 0, 0, 0}};
+    grid = {{ 0, 0, 8, 0, 5, 0, 0, 0, 0},
+            { 1, 0, 0, 9, 2, 0, 0, 0, 6},
+            { 0, 6, 0, 0, 0, 0, 0, 7, 0},
+            { 0, 0, 4, 0, 0, 0, 8, 0, 0},
+            { 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            { 2, 0, 0, 1, 6, 0, 0, 0, 7},
+            { 0, 0, 2, 3, 9, 0, 0, 4, 0},
+            { 0, 0, 0, 0, 0, 5, 0, 0, 9},
+            { 3, 0, 0, 0, 0, 7, 0, 0, 0}};
 
 
     for (int i = 0; i < 9; i++)
@@ -52,12 +55,16 @@ int32_t main() {
 
     sort(faka.begin(), faka.end());
 
+    // for (auto[x, y] : faka) cout << x <<' ' << y << endl;
+
     if(sudoku(0)) {
         cout << "Sudoku solved" << endl;
         for (auto v : grid) {
             for (auto i : v) cout << i << ' ';
                 cout << endl;
         }
+    } else {
+        cout << "The sudoku is not solvable" << endl;
     }
 
     return 0;
