8d7167733d9f57806e5724648d19be24a2cdbfdd
MNnazrul <mnnazrulislam196130@gmail.com>
2023-11-14 08:52:36 +0600

    add

--- Keyword_Based_QA.cpp
+++ Keyword_Based_QA.cpp
@@ -8,7 +8,7 @@ using namespace __gnu_cxx;
 template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
  
 #define int long long
-#define endl "\n"
+// #define endl "\n"
 #define vi vector<int>
 #define pb push_back
 #define size(a) (int)a.size()
@@ -31,25 +31,53 @@ void solve() {
     map<string, string> keywords;
     keywords["you"] = "my";
     keywords["your"] = "your";
+    keywords["are"] = "am";
 
     vector<string> sentences[103];
     int i1 = 0;
     while(input >> s) {
         int n = size(s);
-        sentences[i1].pb(s);
+        for (int i = 0; i < n; i++) s[i] = tolower(s[i]);
+        sentences[i1].pb((s[n-1] == '.' ? s.substr(0, n-1) : s));
         if(s[n-1] == '.')
             i1++;
     }
 
-    // cin >> s;
+    while(1) {
 
-    // while(1) {
         string s1;
         getline(cin, s1);
         cout << s1 << endl;   
-    //     getline(cin, s1); 
-    //     getline(cin, s1); 
-    // // }
+
+        vector<string> str, ans;
+        int count = 0;
+        string st;
+        for (char ch : s1) {
+            ch = tolower(ch);
+            if(!(ch >= 'a' and ch <= 'z')) {
+                str.pb(st);
+                st = "";
+                continue;
+            } 
+            st += ch;
+        }
+
+        for (auto v : sentences) {
+            int cnt = 0;
+            for (string s4 : str) {
+                for (string s5 : v) 
+                    if(s5 == s4) cnt++;
+            }
+            if(count < cnt) {
+                count = cnt;
+                ans = v;
+            }
+        }
+
+        for (string s6 : ans)
+            cout << s6 << ' '; cout << endl;
+
+    }
 
 }
  
